b4e55e12a8d3f0d383f23825da2c24ec3a946153
coopTRUE <coopdog4545@gmail.com>
2022-12-09 23:17:36 -0800

    staring work on database

---
+++ src/lib/server/server.ts
@@ -0,0 +1,22 @@
+// prisma
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+prisma.$connect().then(() => {
+  console.log('Database connected!')
+})
+
+async function login(address: string, signature: string, ip: string) {
+  return await prisma.user.upsert({
+    where: { signature },
+    update: {
+      logins: {
+        create: { ip }
+      }
+    },
+    create: {
+      signature,
+      address
+    }
+  })
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import 'normalize.css'
+  import '../global.scss'
+  import Header from './Header.svelte'
+  import Footer from './Footer.svelte'
 </script>
 
+<Header />
 <slot />
+<Footer />
