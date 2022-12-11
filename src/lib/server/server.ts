a5687bca25ce8474f5be3e2e213c065c37a41c06
coopTRUE <coopdog4545@gmail.com>
2022-12-10 21:04:22 -0800

    completed most of server

--- src/lib/server/server.ts
+++ src/lib/server/server.ts
@@ -1,38 +1,51 @@
 import { PrismaClient } from '@prisma/client'
 import { CRASH_SPEED } from '$lib/constants'
-import { broadcast } from './ws'
-import * as env from '$env/static/private'
+import { ABLY_API_KEY, REAL_GAME } from '$env/static/private'
+import * as Ably from 'ably'
+
+type GameStatus = 'playing' | 'waiting' | 'crashed'
 
 const prisma = new PrismaClient()
 await prisma.$connect()
 console.log('Connected to database')
+const ably = new Ably.Realtime(ABLY_API_KEY)
+const gameStatusChannel = ably.channels.get('gameStatus')
+ably.connection.on('connected', () => console.log('Connected to Ably'))
 
-let gameStatus: 'playing' | 'waiting' | 'crashed' = 'waiting'
+let gameStatus: GameStatus = 'waiting'
 let multiplier = 0
 let stopped = false
 setTimeout(() => (stopped = true), 100000)
 if (+env.REAL_GAME) startGame()
 
 export async function startGame() {
+  console.log('Starting game')
   while (!stopped) {
-    gameStatus = 'waiting'
+    updateGameStatus('waiting')
     await new Promise((resolve) => setTimeout(resolve, 10000))
     await round([])
   }
 }
 
+export async function updateGameStatus(
+  newGameStatus: 'playing' | 'waiting' | 'crashed',
+  multiplier = 0
+) {
+  gameStatus = newGameStatus
+  gameStatusChannel.publish('update', { gameStatus, multiplier })
+  console.log('Status:', gameStatus, multiplier)
+}
+
 export async function round(bets: number[]) {
-  console.log('Starting round')
-  gameStatus = 'playing'
+  updateGameStatus('playing')
   const maxMultiplier = Math.random() * 100
   const duration = maxMultiplier / CRASH_SPEED
   for (let i = 0; i < duration; i++) {
     multiplier = Math.floor(i * CRASH_SPEED * 100) / 100
-    console.log('Multiplier', multiplier)
+    updateGameStatus('playing', multiplier)
     await new Promise((resolve) => setTimeout(resolve, 1000))
   }
-  console.log('Round ended with multiplier', multiplier)
-  gameStatus = 'crashed'
+  updateGameStatus('crashed')
   await prisma.round.create({
     data: {
       maxMultiplier,
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
   import 'normalize.css'
   import '../global.scss'
+  import { PUBLIC_ABLY_KEY } from '$env/static/public'
+  import * as Ably from 'ably'
+  import { browser } from '$app/environment'
+  if (browser) {
+    const ably = new Ably.Realtime(PUBLIC_ABLY_KEY)
+    const channel = ably.channels.get('gameStatus')
+    channel.subscribe('update', (message) => {
+      console.log(message)
+    })
+  }
 </script>
 
 <slot />
